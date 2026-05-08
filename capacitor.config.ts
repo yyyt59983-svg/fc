cae898887bd12edb5c12d5d1a877d61b8e4f5491
yyyt59983-svg <yyyt59983@gmail.com>
2026-05-08 21:44:45 +0530

    Build: Added Electron and Capacitor configurations for Windows and Android

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.aegis.tactical',
+  appName: 'FC',
+  webDir: 'dist'
+};
+
+export default config;
---
+++ electron/main.cjs
@@ -0,0 +1,38 @@
+const { app, BrowserWindow } = require('electron');
+const path = require('path');
+const isDev = process.env.NODE_ENV === 'development';
+
+function createWindow() {
+  const win = new BrowserWindow({
+    width: 450,
+    height: 850,
+    webPreferences: {
+      nodeIntegration: true,
+      contextIsolation: false,
+    },
+    frame: false, // Tactical borderless look
+    transparent: true,
+    resizable: false,
+    icon: path.join(__dirname, '../public/aegis-icon.png')
+  });
+
+  if (isDev) {
+    win.loadURL('http://localhost:5173');
+  } else {
+    win.loadFile(path.join(__dirname, '../dist/index.html'));
+  }
+}
+
+app.whenReady().then(createWindow);
+
+app.on('window-all-closed', () => {
+  if (process.platform !== 'darwin') {
+    app.quit();
+  }
+});
+
+app.on('activate', () => {
+  if (BrowserWindow.getAllWindows().length === 0) {
+    createWindow();
+  }
+});
